3f097326e303da0ef83b5c6a5ede6d99a26f24b6
PERRIER Aurélien <perrie_a@etna-alternance.net>
2015-12-13 00:20:55 +0100

    laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::group(['middleware' => 'oauth'], function () {
-
-});*/
 
 Route::get('/api/auth/{user}/{token}', 'Auth\AuthController@getLogin', function ($tokenId) {
 	Route::controller(
@@ -29,13 +26,7 @@ Route::get('/api/auth/{user}/{token}', 'Auth\AuthController@getLogin', function 
 
 Route::get('/api/logout', 'Auth\AuthController@getLogout');
 
-/* 
- * ex : localhost:8000/api/search/PALUM?flux=0&country=FR&time=0
- * show, create, modify, create
- * date : multiple format => 21-12-1995, tri, month, year, year^year, month-year^month-year
- * Country => fr
- * redirection si non auth => / (middleware/auth)
- */
+
 Route::group(['middleware' => 'auth.api'], function () {
 	Route::get('/api/{meth}/{mat}', 'API@index', function ($matId) {
 		Route::controller('APIctrl', 'API', ['meth' => 'APIctrl.index', 'mat' => 'APIctrl.index']);
